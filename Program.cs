8d55ab7824e78e2e3bca6c2650cfd46c2cc5d55f
Spencer G. Jones <spencer.jones2@tylertech.com>
2024-10-28 06:38:19 -0700

    Convert to JsonNode

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
@@ -11,10 +12,10 @@ var terraformPolicyFileNames = args.Skip(1);
 var planStream = new FileStream(terraformPlanFileName, FileMode.Open, FileAccess.Read);
 var inputPolicies = terraformPolicyFileNames.Select(GetPolicy).ToArray();
 var policy = new Policy {
-	permittedCreates = inputPolicies.SelectMany(i => i.permittedCreates).ToArray(),
-	permittedDeletes = inputPolicies.SelectMany(i => i.permittedDeletes).ToArray(),
-	permittedUpdates = inputPolicies.SelectMany(i => i.permittedUpdates).ToArray(),
-	permittedDrifts = inputPolicies.SelectMany(i => i.permittedDrifts).ToArray(),
+	permittedCreates = inputPolicies.SelectMany(i => i.permittedCreates ?? new PolicyElement[0]).ToArray(),
+	permittedDeletes = inputPolicies.SelectMany(i => i.permittedDeletes ?? new PolicyElement[0]).ToArray(),
+	permittedUpdates = inputPolicies.SelectMany(i => i.permittedUpdates ?? new PolicyElement[0]).ToArray(),
+	permittedDrifts = inputPolicies.SelectMany(i => i.permittedDrifts ?? new PolicyElement[0]).ToArray(),
 };
 [System.Diagnostics.CodeAnalysis.UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026:RequiresUnreferencedCode", Justification = "Supplying JsonSerializerOptions with TypeInfoResolver should suppress this warning. This appears to be a bug?")]
 static Policy GetPolicy(string terraformPolicyFileName) {
@@ -23,17 +24,18 @@ static Policy GetPolicy(string terraformPolicyFileName) {
 }
 //Console.WriteLine(J(policy));
 
-var document = System.Text.Json.JsonDocument.Parse(planStream, new JsonDocumentOptions { AllowTrailingCommas = true, CommentHandling = JsonCommentHandling.Skip });
+var document = AssertNotNull(System.Text.Json.Nodes.JsonNode.Parse(planStream, null, new JsonDocumentOptions { AllowTrailingCommas = true, CommentHandling = JsonCommentHandling.Skip }));
 var failed = false;
 
-var changes = FindChild(document.RootElement, "resource_changes")?.EnumerateArray().ToArray() ?? new JsonElement[0];
-foreach (var drift in FindChild(document.RootElement, "resource_drift")?.EnumerateArray().ToArray() ?? new JsonElement[0]) {
-	var address = AssertNotNull(drift.GetProperty("address").GetString());
-	var type = AssertNotNull(drift.GetProperty("type").GetString());
-	var change = AssertNotNullS(FindChild(drift, "change"));
-	var before = AssertNotNullS(FindChild(change, "before"));
-	var after = AssertNotNullS(FindChild(change, "after"));
-	var actions = GetStringArray(AssertNotNullS(FindChild(change, "actions")));
+var changes = FindChild(document, "resource_changes")?.AsArray().ToArray() ?? new JsonNode[0];
+foreach (var drift in FindChild(document, "resource_drift")?.AsArray().ToArray() ?? new JsonNode[0]) {
+	AssertNotNull(drift);
+	var address = AssertNotNull(AssertNotNull(drift["address"]).GetValue<string>());
+	var type = AssertNotNull(AssertNotNull(drift["type"]).GetValue<string>());
+	var change = AssertNotNull(FindChild(drift, "change"));
+	var before = AssertNotNull(FindChild(change, "before"));
+	var after = AssertNotNull(FindChild(change, "after"));
+	var actions = GetStringArray(AssertNotNull(FindChild(change, "actions")));
 	if (!Enumerable.SequenceEqual(actions, new[] { "update" })) {
 		Console.WriteLine($"Drift unknown action: {string.Join(", ", actions)}");
 		failed = true;
@@ -45,7 +47,7 @@ foreach (var drift in FindChild(document.RootElement, "resource_drift")?.Enumera
 	var policies = GetPolicyElements(policy.permittedDrifts, address, type).SelectMany(i => i.permittedUpdates ?? new PolicyItem[0]).ToArray();
 	//Console.WriteLine(J(policies));
 	Console.WriteLine($"Drift diff: {address} - {type}");
-	if (changes.Any(i => i.GetProperty("address").GetString() == address && GetStringArray(AssertNotNullS(FindChild(AssertNotNullS(FindChild(i, "change")), "actions"))).Contains("delete"))) {
+	if (changes.Any(i => AssertNotNull(AssertNotNull(i)["address"]).GetValue<string>() == address && GetStringArray(AssertNotNull(FindChild(AssertNotNull(FindChild(i, "change")), "actions"))).Contains("delete"))) {
 		Console.WriteLine($"Ignoring drift on resource being deleted");
 		continue;
 	}
@@ -63,10 +65,11 @@ foreach (var drift in FindChild(document.RootElement, "resource_drift")?.Enumera
 }
 
 foreach (var changeI in changes) {
-	var address = changeI.GetProperty("address").GetString()!;
-	var type = changeI.GetProperty("type").GetString()!;
-	var change = AssertNotNullS(FindChild(changeI, "change"));
-	var actions = GetStringArray(AssertNotNullS(FindChild(change, "actions")));
+	AssertNotNull(changeI);
+	var address = AssertNotNull(AssertNotNull(changeI["address"]).GetValue<string>());
+	var type = AssertNotNull(AssertNotNull(changeI["type"]).GetValue<string>());
+	var change = AssertNotNull(FindChild(changeI, "change"));
+	var actions = GetStringArray(AssertNotNull(FindChild(change, "actions")));
 
 	if (Enumerable.SequenceEqual(actions, new[] { "no-op" }) || Enumerable.SequenceEqual(actions, new[] { "read" })) {
 		continue;
@@ -78,8 +81,8 @@ foreach (var changeI in changes) {
 			Console.WriteLine($"     DENIED");
 			failed = true;
 		} else {
-			var before = AssertNotNullS(FindChild(change, "before"));
-			var after = AssertNotNullS(FindChild(change, "after"));
+			var before = AssertNotNull(FindChild(change, "before"));
+			var after = AssertNotNull(FindChild(change, "after"));
 			var diffs = Diff(type, "", before, after);
 			var itemPolicies = policies.SelectMany(i => i.permittedUpdates ?? new PolicyItem[0]);
 			foreach (var diff in diffs) {
@@ -121,7 +124,19 @@ foreach (var changeI in changes) {
 Console.WriteLine($"Final Outcome: {(failed ? "DENIED" : "Allowed")}");
 return failed ? 1 : 0;
 
-static bool ElementMatch(DiffItem diffItem, PolicyItem policyItem, JsonElement beforeRoot, JsonElement afterRoot) {
+//static object MaskSensitive(string path, JsonNode input, JsonNode sensitiveSource) {
+//	var sensitiveMask = FindElement(sensitiveSource, path);
+//	if (sensitiveMask == null) {
+//		return input;
+//	}
+//	var masked = input.Clone();
+//	input.TryGetProperty
+//	if (sensitiveMask.Value.ValueKind == JsonValueKind.True) {
+//	}
+//	return input;
+//}
+
+static bool ElementMatch(DiffItem diffItem, PolicyItem policyItem, JsonNode beforeRoot, JsonNode afterRoot) {
 	var matchA = Regex.Match(diffItem.address, MakeAllStringMatch(policyItem.addressRegex));
 	var match = matchA.Success && (policyItem.beforeRegex == null || Regex.IsMatch(diffItem.before, MakeAllStringMatch(policyItem.beforeRegex))) && (policyItem.afterRegex == null || Regex.IsMatch(diffItem.after, MakeAllStringMatch(policyItem.afterRegex)));
 	if (match) {
@@ -129,8 +144,8 @@ static bool ElementMatch(DiffItem diffItem, PolicyItem policyItem, JsonElement b
 			var address = Regex.Replace(whenItem.address, "\\$(\\d+)", (m) => {
 				return matchA.Groups[int.Parse(m.Groups[1].Value)].Value;
 			});
-			var beforeElement = FindElement(beforeRoot, address);
-			var afterElement = FindElement(afterRoot, address);
+			var beforeElement = AssertNotNull(FindElement(beforeRoot, address));
+			var afterElement = AssertNotNull(FindElement(afterRoot, address));
 			match = (whenItem.beforeRegex == null || Regex.IsMatch(beforeElement.ToString()!, MakeAllStringMatch(whenItem.beforeRegex))) && (whenItem.afterRegex == null || Regex.IsMatch(afterElement.ToString()!, MakeAllStringMatch(whenItem.afterRegex)));
 			if (!match) {
 				Console.WriteLine($"When: DIDN'T match {address}: {beforeElement.ToString()!} != {whenItem.beforeRegex} or {afterElement.ToString()!} != {whenItem.afterRegex}");
@@ -142,7 +157,7 @@ static bool ElementMatch(DiffItem diffItem, PolicyItem policyItem, JsonElement b
 	}
 	return match;
 }
-static JsonElement FindElement(JsonElement root, string path) {
+static JsonNode? FindElement(JsonNode root, string path) {
 	if (path == "") {
 		return root;
 	}
@@ -151,12 +166,12 @@ static JsonElement FindElement(JsonElement root, string path) {
 	var key = nextIndex == -1 ? path.Substring(1) : path.Substring(1, nextIndex - 1);
 	if (path[0] == '.') {
 		var remainder = nextIndex == -1 ? "" : path.Substring(nextIndex);
-		return FindElement(AssertNotNullS(FindChild(root, key)), remainder);
+		return FindElement(AssertNotNull(FindChild(root, key)), remainder);
 	} else if (path[0] == '[') {
 		var remainder = nextIndex == -1 ? "" : path.Substring(nextIndex + 1);
-		return FindElement(AssertNotNullS(FindChildArray(root, int.Parse(key))), remainder);
+		return FindElement(AssertNotNull(FindChildArray(root, int.Parse(key))), remainder);
 	} else {
-		throw new Exception($"Unknown path: {path}");
+		return null;
 	}
 }
 static PolicyElement[] GetPolicyElements(PolicyElement[]? list, string address, string type) {
@@ -169,20 +184,14 @@ static PolicyElement[] GetPolicyElements(PolicyElement[]? list, string address, 
 
 static string MakeAllStringMatch(string regex) => $"^(?:{regex})$";
 //static string J(object o) => JsonSerializer.Serialize(o, new JsonSerializerOptions { AllowTrailingCommas = true, ReadCommentHandling = JsonCommentHandling.Skip, IncludeFields = true, WriteIndented = true });
-static JsonElement? FindChild(JsonElement el, string name) {
-	return el.EnumerateObject().Cast<JsonProperty?>().FirstOrDefault(i => i!.Value.Name == name)?.Value;
+static JsonNode? FindChild(JsonNode el, string name) {
+	return el.AsObject()[name];
 };
 
-static JsonElement? FindChildArray(JsonElement el, int index) {
-	return el.EnumerateArray().Skip(index).Cast<JsonElement?>().FirstOrDefault();
+static JsonNode? FindChildArray(JsonNode el, int index) {
+	return el.AsArray().Count < index ? el.AsArray()[index] : null;
 };
-static string[] GetStringArray(JsonElement el) => el.EnumerateArray().Select(i => i.GetString()!).ToArray();
-static T AssertNotNullS<T>([System.Diagnostics.CodeAnalysis.NotNull] T? input, [System.Runtime.CompilerServices.CallerArgumentExpressionAttribute(nameof(input))] string inputStr = null!) where T : struct {
-	if (input == null) {
-		throw new ArgumentNullException(inputStr);
-	}
-	return input.Value;
-}
+static string[] GetStringArray(JsonNode el) => el.AsArray().Select(i => AssertNotNull(i).AsValue().GetValue<string>()).ToArray();
 static T AssertNotNull<T>([System.Diagnostics.CodeAnalysis.NotNull] T? input, [System.Runtime.CompilerServices.CallerArgumentExpressionAttribute(nameof(input))] string inputStr = null!) {
 	if (input == null) {
 		throw new ArgumentNullException(inputStr);
@@ -192,42 +201,45 @@ static T AssertNotNull<T>([System.Diagnostics.CodeAnalysis.NotNull] T? input, [S
 //[System.Diagnostics.CodeAnalysis.DoesNotReturn]
 //static void ThrowE(string label, string address) => throw new Exception($"{label} at {address}");
 
-static IEnumerable<DiffItem> Diff(string type, string address, JsonElement? before, JsonElement? after) {
-	if (before == null || after == null || before.Value.ValueKind != after.Value.ValueKind) {
+static IEnumerable<DiffItem> Diff(string type, string address, JsonNode? before, JsonNode? after) {
+	if (before == null || after == null || before.GetValueKind() != after.GetValueKind()) {
 		if (before == null) {
-			yield return new DiffItem(address, "NULL", AssertNotNull(after.ToString()));
+			if (after == null) {
+			} else {
+				yield return new DiffItem(address, "NULL", AssertNotNull(after.ToString()));
+			}
 		} else if (after == null) {
 			yield return new DiffItem(address, AssertNotNull(before.ToString()), "NULL");
 		} else {
-			yield return new DiffItem(address, before.Value.ValueKind.ToString(), after.Value.ValueKind.ToString());
+			yield return new DiffItem(address, before.GetValueKind().ToString(), after.GetValueKind().ToString());
 		}
 		yield break;
 	}
-	var beforeV = before.Value;
-	var afterV = after.Value;
+	var beforeV = before;
+	var afterV = after;
 	if (type == "helm_release" && address == ".set") {
-		static string getName(JsonElement i) => AssertNotNullS(FindChild(i, "name")).GetString()!;
-		var beforeItemsD = beforeV.EnumerateArray().ToArray().ToLookup(getName).ToDictionary(i => i.Key, i => i.ToArray());
-		var afterItemsD = afterV.EnumerateArray().ToArray().ToLookup(getName).ToDictionary(i => i.Key, i => i.ToArray());
+		static string getName(JsonNode? i) => AssertNotNull(FindChild(AssertNotNull(i), "name")).AsValue().GetValue<string>();
+		var beforeItemsD = beforeV.AsArray().ToArray().ToLookup(getName).ToDictionary(i => i.Key, i => i.ToArray());
+		var afterItemsD = afterV.AsArray().ToArray().ToLookup(getName).ToDictionary(i => i.Key, i => i.ToArray());
 		var keys = beforeItemsD.Concat(afterItemsD).Select(i => i.Key).Distinct();
 		foreach (var key in keys) {
-			var beforeItems = (beforeItemsD.TryGetValue(key, out var a1) ? a1 : null) ?? new JsonElement[0];
-			var afterItems = (afterItemsD.TryGetValue(key, out var a2) ? a2 : null) ?? new JsonElement[0];
+			var beforeItems = (beforeItemsD.TryGetValue(key, out var a1) ? a1 : null) ?? new JsonNode[0];
+			var afterItems = (afterItemsD.TryGetValue(key, out var a2) ? a2 : null) ?? new JsonNode[0];
 			var count = Math.Max(beforeItems.Length, afterItems.Length);
 			for (var i = 0; i < count; i++) {
 				foreach (var j in Diff(type, $"{address}[{key}][{i}]", i < beforeItems.Length ? beforeItems[i] : null, i < afterItems.Length ? afterItems[i] : null)) { yield return j; }
 			}
 		}
-	} else if (beforeV.ValueKind == JsonValueKind.Array) {
-		var beforeItems = beforeV.EnumerateArray().ToArray();
-		var afterItems = afterV.EnumerateArray().ToArray();
+	} else if (beforeV is JsonArray beforeA) {
+		var beforeItems = beforeA.ToArray();
+		var afterItems = afterV.AsArray().ToArray();
 		var count = Math.Max(beforeItems.Length, afterItems.Length);
 		for (var i = 0; i < count; i++) {
 			foreach (var j in Diff(type, $"{address}[{i + 1}]", i < beforeItems.Length ? beforeItems[i] : null, i < afterItems.Length ? afterItems[i] : null)) { yield return j; }
 		}
-	} else if (beforeV.ValueKind == JsonValueKind.Object) {
-		var beforeItems = beforeV.EnumerateObject().ToDictionary(i => i.Name, i => i.Value);
-		var afterItems = afterV.EnumerateObject().ToDictionary(i => i.Name, i => i.Value);
+	} else if (beforeV is JsonObject beforeO ) {
+		var beforeItems = beforeO.ToDictionary(i => i.Key, i => i.Value);
+		var afterItems = afterV.AsObject().ToDictionary(i => i.Key, i => i.Value);;
 		var keys = beforeItems.Keys.Union(afterItems.Keys);
 		foreach (var key in keys) {
 			foreach (var j in Diff(type, $"{address}.{key}", beforeItems.TryGetValue(key, out var bI) ? bI : null, afterItems.TryGetValue(key, out var aI) ? aI : null)) { yield return j; }
